3d5cf63098cee8752c34890a0c79a23cf9c37da2
wiiwake3101 <vm4512@srmist.edu.in>
2026-03-17 14:26:37 +0530

    Removed Cursor

--- app/page.jsx
+++ app/page.jsx
@@ -60,7 +60,6 @@ export default function Home() {
 
       {/* UI chrome */}
       <ScrollProgress />
-      <CyberpunkCursor />
       <BackToTop />
       <FloatingSocial />
       <BootScreen />
